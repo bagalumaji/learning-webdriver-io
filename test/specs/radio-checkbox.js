49db009e7a1bd70ff6315d3dfd81ca82f8eef532
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-07-06 11:04:22 +0530

    test updated

--- test/specs/radio-checkbox.js
+++ test/specs/radio-checkbox.js
@@ -2,10 +2,6 @@ describe('RadioCheckbox', function () {
     it('radio-checkbox select test', async () => {
         await browser.url('https://www.lambdatest.com/selenium-playground/checkbox-demo')
         await browser.maximizeWindow();
-        // const checkboxLocator = await $('#isAgeSelected');
-        // await checkboxLocator.click();
-        // const isClickableCheckbox = await checkboxLocator.isEnabled();
-        // console.log("isClickableCheckbox : ", isClickableCheckbox);
         const listList = await $$("//*[text()='Multiple Checkbox Demo']/following-sibling::div//input[@type='checkbox']")
 
         const timeout = await browser.getTimeouts();
