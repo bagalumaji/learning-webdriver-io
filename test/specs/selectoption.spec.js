fe7fed87ec3815714c361cfed5898a0f1bf0c016
Umaji Bagal <bagalumaji@gmail.com>
2025-07-16 22:02:45 +0530

    added select option

---
+++ test/specs/selectoption.spec.js
@@ -0,0 +1,15 @@
+import {browser} from "@wdio/globals";
+
+describe.only('Select Option', () => {
+    it.only('select option test', async () => {
+        await browser.url("https://testautomationpractice.blogspot.com/")
+        const select_locator = $("#country")
+        await select_locator.selectByVisibleText("Japan");
+        await browser.pause(5000);
+        await select_locator.selectByAttribute("value","uk")
+        await browser.pause(5000);
+        await select_locator.selectByIndex(5)
+        await browser.pause(5000);
+
+    })
+})
